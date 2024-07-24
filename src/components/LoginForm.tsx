6338f9227e738e145fe426185d857a32e3891ffe
ahadsts9901 <ahadsts990@gmail.com>
2024-07-24 14:42:18 +0500

    ap

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import ReCAPTCHA from 'react-google-recaptcha';
 import useRecaptcha from '../hooks/useRecaptcha';
-import axios from "axios"
 
 const LoginForm = () => {
   const { capchaToken, recaptchaRef, handleRecaptcha } = useRecaptcha();
@@ -14,32 +13,10 @@ const LoginForm = () => {
     e?.preventDefault();
     if (capchaToken) {
 
-      const result = await axios.post(`https://social-media-web-0lwr.onrender.com/api/v1/login`, {
-        email: username,
-        password: password,
-        capchaToken,
-      });
-
-      if (result.data.recaptchaValid === false) {
-        alert('ReCAPTCHA validation failed. Please try again.');
-        handleRecaptcha('');
-        if (recaptchaRef.current) {
-          recaptchaRef.current.reset();
-        }
-        return;
-      }
+      alert("captcha successfull")
 
       recaptchaRef.current?.reset();
 
-      if (result.data.success) {
-
-        console.log('Login successful');
-
-      } else {
-
-        alert('Login failed. Please check your credentials and try again.');
-
-      }
     } else {
       alert('Please fill in all fields and complete the captcha.');
     }
@@ -67,7 +44,7 @@ const LoginForm = () => {
       <br />
       <ReCAPTCHA
         ref={recaptchaRef}
-        sitekey="Your client site key"
+        sitekey="6LctRBcqAAAAAAXwvJCVqk3GTWjpTbRP2wAUQgv5"
         onChange={handleRecaptcha}
       />
       <br />
